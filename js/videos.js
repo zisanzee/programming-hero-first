177598586ddca8f31715c02273e55be08ce85f75
Towhid Hossain <zisankhanchowdhury@gmail.com>
2023-07-07 14:46:32 +0600

    added new videos

--- js/videos.js
+++ js/videos.js
@@ -8,7 +8,91 @@ const video = [
   },
   {
     thumbnail: "https://i.ytimg.com/vi/iFv6BXjnKRU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARU",
-    title: 'YNW Melly ft. Kanye West - Mixed Personalities (Directed by Cole Bennett',
+    title: 'YNW Melly ft. Kanye West - Mixed Personalities (Directed by Cole Bennett)',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/tjMp_IMSAF0/hq720.jpg?sqp=-oaymwE",
+    title: 'My SKY FACTORY is GROWING Minecraft ft. Jesse (Ep.3',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/ZW3dcu8H4gI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDdNJeDLn811bCR_HQbs5FSUa1AKA",
+    title: 'Textbooks Are All You Need',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "	https://i.ytimg.com/vi/-r4Xcns47YA/hq720.jpg?sqp=-…Rh_IBMoSDAP&rs=AOn4CLD3cF9Up5nOukoeXI4i8MFK-CavFA",
+    title: 'Baby Keem - Honest (Official Video)',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "	https://i.ytimg.com/vi/1UTjWy-vnOo/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDQdn12sWvtIfmBR15xU-GVMnVMyw",
+    title: 'I Gave the MIT Commencement Speech for 2023',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "	https://i.ytimg.com/vi/Cv0N8g68t2w/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLD40VxUCb85QbxHUVjHGeOEg84PZg",
+    title: 'when a gang leader confronted mike tyson',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "	https://i.ytimg.com/vi/ihNjCJi7aeY/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCH_SQFGlcE7Jcef-gHS7DJQFIwPg",
+    title: "Gmod Prop Hunt Funny Moments - Toy Story 4 Edition (Garry's mod)",
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/AJuRE_dnABM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDhdahxBmmRn0C7q77JBu-6TkUfHQ",
+    title: 'Hydra getting smoked',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/iFv6BXjnKRU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARU",
+    title: 'YNW Melly ft. Kanye West - Mixed Personalities (Directed by Cole Bennett)',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/tjMp_IMSAF0/hq720.jpg?sqp=-oaymwE",
+    title: 'My SKY FACTORY is GROWING Minecraft ft. Jesse (Ep.3',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/AJuRE_dnABM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDhdahxBmmRn0C7q77JBu-6TkUfHQ",
+    title: 'idfk',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/AJuRE_dnABM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDhdahxBmmRn0C7q77JBu-6TkUfHQ",
+    title: 'Hydra getting smoked',
+    channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
+    channelName: 'ZisanZee',
+    videoViews: '410k &#x2022; 10 hours ago'
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/iFv6BXjnKRU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARU",
+    title: 'YNW Melly ft. Kanye West - Mixed Personalities (Directed by Cole Bennett)',
     channelPfp: "https://media.istockphoto.com/id/850704730/photo/young-serious-guy-portrait-on-black-background-black-and-white-photo.jpg?s=612x612&w=0&k=20&c=lBJuY0SnTustfYp5zgWcYxn8_C61iHbFWweNNh83X1E=",
     channelName: 'ZisanZee',
     videoViews: '410k &#x2022; 10 hours ago'
